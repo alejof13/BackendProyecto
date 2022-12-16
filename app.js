3df0f4a088ab4fddfe486e74c4390c9a3ade8e4b
Alejandro <alejofu13@gmail.com>
2022-12-16 10:29:09 -0600

    Seccion 5

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/api',user_routes);
+app.use('/api',artist_routes);
 
 module.exports= app;
